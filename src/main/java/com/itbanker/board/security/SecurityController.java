f4e5219415580643273ef02a31fd880a9d2e73a2
Garfield <iju1633@gmail.com>
2022-01-13 14:56:08 +0900

    주석추가 - 토큰 구현 - 토큰 검증 - 간단하게 controller 구현해서 postman 으로 test - jwt.io 에서 token 검증 가능

--- src/main/java/com/itbanker/board/security/SecurityController.java
+++ src/main/java/com/itbanker/board/security/SecurityController.java
@@ -16,8 +16,8 @@ public class SecurityController {
     @Autowired
     private SecurityService securityService;
 
-    @GetMapping("/create/token")
-    public Map<String, Object> createToken(@RequestParam(value = "subject") String subject) {
+    @GetMapping("/create/token") // @RequestParam을 통해 url 에서 subject?=kang 이런 식으로 설정 가능
+    public Map<String, Object> createToken(@RequestParam(value = "subject") String subject) { // (key, value) 형태이고 val ue가 json 형식이니까 Object 로 설정
         String token = securityService.createToken(subject, (2*1000*60));
         Map<String, Object> map = new LinkedHashMap<>();
         map.put("result", token);
@@ -34,3 +34,8 @@ public class SecurityController {
         return map;
     }
 }
+
+// postman 에서 test 진행
+// token 의 형태를 보면 ~~~.~~~.~~~ 이렇게 되어 있는데, 첫 번째는 header, 두 번째는 payload(claim),
+// 세 번째는 header 와 payload 를 인코딩한 값을 key로 해싱시켜서 만들어낸 서명값에 해당한다.
+// jwt.io 라는 사이트에서 암호키 값을 넣어 검증이 가능함!
--- src/main/java/com/itbanker/board/security/SecurityService.java
+++ src/main/java/com/itbanker/board/security/SecurityService.java
@@ -12,34 +12,42 @@ import java.util.Date;
 
 @Service
 public class SecurityService {
+    // 실제로는 이렇게 비밀키를 만들면 안됨!
     private static final String SECRET_KEY = "aslsdisdjfiasojfoiasjfoaskfmsdklfaskldfajrhwqieobfwqegwoqifbiwoqbfiwoqfeqsd";
 
     // 로그인 서비스 던질 때 같이 ~~
+    // 토큰을 구현하는 메서드!
     public String createToken(String subject, long expTime) {
         if(expTime <= 0) {
-            throw new RuntimeException("you should login again");
+            throw new RuntimeException("만료시간이 0보다 커야합니다.");
         }
 
+        // 서명 알고리즘을 구현하기 위해 HS256이라는 알고리즘 선택함
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
+        // String 형태의 key 를 byte 로 전환해줌
+        // implementation 'javax.xml.bind:jaxb-api' 에서 DatatypeConverter 를 활용
         byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        // 암호화 키를 만듦
         Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());
 
+        // 만든 키를 return
         return Jwts.builder()
-                .setSubject(subject)
+                .setSubject(subject) // user 의 id 값을 subject 로 받고, 비밀번호로 SECRET_KEY 를 만드는 데 사용(강사님 생각)
                 .signWith(signingKey, signatureAlgorithm)
-                .setExpiration(new Date(System.currentTimeMillis() + expTime))
+                .setExpiration(new Date(System.currentTimeMillis() + expTime)) // 실제로는 헤더 값 등 다양한 정보를 넣을 수 있음
                 .compact();
     }
 
     // 토큰 검증하는 메서드를 boolean ~~
+    // 원래는 내부에서 인증되어있는 토큰인 지 파악하도록 작성함
     public String getSubject(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+        Claims claims = Jwts.parserBuilder() // Claims 는 pay load 안에 담겨있는 정보
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY)) // 키를 setting
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseClaimsJws(token) // token 을 넣어서 풀어줌
+                .getBody(); // 풀어준 값을 가져옴
 
-        return claims.getSubject();
+        return claims.getSubject(); // 풀어준 claims 안에 있는 정보 중, subject 만 가져옴
     }
 }
